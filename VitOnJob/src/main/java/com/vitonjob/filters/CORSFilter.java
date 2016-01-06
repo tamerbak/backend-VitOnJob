a21115a9f4ee054f056369ecc8edca5ecb82d0db
rachidchaaba <rachid.chaaba@gmail.com>
2016-01-06 00:45:26 +0000

    web service de l'authentification

--- VitOnJob/src/main/java/com/vitonjob/filters/CORSFilter.java
+++ VitOnJob/src/main/java/com/vitonjob/filters/CORSFilter.java
@@ -12,7 +12,7 @@ public class CORSFilter implements ContainerResponseFilter{
 
         response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHttpHeaders().add("Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
+				"origin, content-type, accept, authorization, login");
         response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHttpHeaders().add("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
